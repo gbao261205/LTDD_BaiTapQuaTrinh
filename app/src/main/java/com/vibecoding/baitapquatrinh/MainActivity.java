fae483e9c7d6839c00e4366fb710f4a2eb86b53e
PHtai16 <23162086@student.hcmute.edu.vn>
2025-11-28 15:40:46 +0700

    main3

--- app/src/main/java/com/vibecoding/baitapquatrinh/MainActivity.java
+++ app/src/main/java/com/vibecoding/baitapquatrinh/MainActivity.java
@@ -1,13 +1,17 @@
-package com.vibecoding.baitapquatrinh; //Phan Văn Tài mssv: 23162086
+package com.vibecoding.baitapquatrinh;
 
+import android.content.Intent;
 import android.os.Bundle;
 import android.os.Handler;
 import android.os.Looper;
+import android.view.MenuItem;
 import android.widget.TextView;
 
+import androidx.annotation.NonNull;
 import androidx.appcompat.app.AppCompatActivity;
 import androidx.viewpager.widget.ViewPager;
 
+import com.google.android.material.bottomnavigation.BottomNavigationView;
 import com.vibecoding.baitapquatrinh.Model.User;
 import com.vibecoding.baitapquatrinh.Service.SessionManager;
 import com.vibecoding.baitapquatrinh.Slider_Images_ViewPager.Images;
@@ -25,6 +29,7 @@ public class MainActivity extends AppCompatActivity {
     private Timer timer;
     private SessionManager sessionManager;
     private TextView tvGreeting;
+    private BottomNavigationView bottomNavigationView;
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
@@ -33,6 +38,7 @@ public class MainActivity extends AppCompatActivity {
 
         viewPager = findViewById(R.id.viewPager);
         tvGreeting = findViewById(R.id.tv_greeting);
+        bottomNavigationView = findViewById(R.id.bottom_navigation);
 
         sessionManager = new SessionManager(this);
 
@@ -40,11 +46,9 @@ public class MainActivity extends AppCompatActivity {
         ImagesViewPagerAdapter adapter = new ImagesViewPagerAdapter(imagesList);
         viewPager.setAdapter(adapter);
 
-        // Bắt đầu auto slide
         autoSlideImages();
-
-        // Lấy và hiển thị tên người dùng
         displayUserName();
+        setupBottomNavigation();
     }
 
     private void displayUserName() {
@@ -54,6 +58,23 @@ public class MainActivity extends AppCompatActivity {
                 tvGreeting.setText("Hi! " + user.getFull_name());
             }
         }
+    }
+
+    private void setupBottomNavigation() {
+        bottomNavigationView.setOnNavigationItemSelectedListener(new BottomNavigationView.OnNavigationItemSelectedListener() {
+            @Override
+            public boolean onNavigationItemSelected(@NonNull MenuItem item) {
+                if (item.getItemId() == R.id.navigation_profile) {
+                    Intent intent = new Intent(MainActivity.this, ProfileActivity.class);
+                    startActivity(intent);
+                    return true;
+                } else if (item.getItemId() == R.id.navigation_home) {
+                    // Already on home screen
+                    return true;
+                }
+                return false;
+            }
+        });
     }
 
     private List<Images> getListImages() {
@@ -91,7 +112,7 @@ public class MainActivity extends AppCompatActivity {
                     }
                 });
             }
-        }, 500, 3000); // Bắt đầu sau 0.5s, lặp lại mỗi 3s
+        }, 500, 3000);
     }
 
     @Override
